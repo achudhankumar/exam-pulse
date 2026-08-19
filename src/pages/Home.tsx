76b9ebb0e22246199749ed0bd6d0f837e3cc43cb
achudhan <achudhankumar@gmail.com>
2026-08-19 16:24:09 +0530

    Remove unused React import from Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { supabase } from '../lib/supabaseClient'
 import { Calendar, Users, BookOpen, Award, ArrowRight, Zap, TrendingUp, Clock } from 'lucide-react'
