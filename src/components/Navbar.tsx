a4232d856d51ad1b3c06f243adf5354705223801
achudhan <achudhankumar@gmail.com>
2026-08-19 15:30:39 +0530

    Fix syntax error in Navbar Leaderboard link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
             <Link to="/" className="hover:underline">Home</Link>
             <Link to="/about" className="hover:underline">About</Link>
             <Link to="/contact" className="hover:underline">Contact</Link>
-	    <Link to="/leaderboard" className="hover:underline">Leaderboard</Link>
+            <Link to="/leaderboard" className="hover:underline">Leaderboard</Link>   {/* <-- fixed */}
             {user ? (
               <>
                 <Link to="/dashboard" className="hover:underline">Dashboard</Link>
