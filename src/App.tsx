f3d7446883086f6f29b5dff132a738ee418e20db
achudhan <achudhankumar@gmail.com>
2026-08-20 10:21:25 +0530

    Fix App.tsx syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,14 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import AdminDashboard from './pages/admin/AdminDashboard'
+import ManageQuizzes from './pages/admin/ManageQuizzes'
+import ManageCategories from './pages/admin/ManageCategories'
+import ManageQuestions from './pages/admin/ManageQuestions'
 import Quiz from './pages/Quiz'
 import Result from './pages/Result'
 import Leaderboard from './pages/Leaderboard'
 import Categories from './pages/Categories'
 import CategoryDetail from './pages/CategoryDetail'
-import ManageQuizzes from './pages/admin/ManageQuizzes'
-import AdminDashboard from './pages/admin/AdminDashboard'
-import AdminDashboard from './pages/admin/AdminDashboard'
-import ManageQuizzes from './pages/admin/ManageQuizzes'
-import ManageCategories from './pages/admin/ManageCategories'
-import ManageQuestions from './pages/admin/ManageQuestions'
 
 function App() {
   return (
@@ -34,18 +31,14 @@ function App() {
             <Route path="/register" element={<Register />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/admin/quizzes" element={<ManageQuizzes />} />
+            <Route path="/admin/categories" element={<ManageCategories />} />
+            <Route path="/admin/quiz/:quizId/questions" element={<ManageQuestions />} />
             <Route path="/quiz/:id" element={<Quiz />} />
             <Route path="/result/:attemptId" element={<Result />} />
-	    <Route path="/leaderboard" element={<Leaderboard />} />
-	    <Route path="/categories" element={<Categories />} />
-	    <Route path="/category/:slug" element={<CategoryDetail />} />
-	    <Route path="/admin/quizzes" element={<ManageQuizzes />} />
-	    <Route path="/admin" element={<AdminDashboard />} />
-	    <Route path="/admin" element={<AdminDashboard />} />
-	    <Route path="/admin/quizzes" element={<ManageQuizzes />} />
-	    <Route path="/admin/categories" element={<ManageCategories />} />
-	    <Route path="/admin/quiz/:quizId/questions" element={<ManageQuestions />} />
-
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/category/:slug" element={<CategoryDetail />} />
           </Routes>
         </main>
         <Footer />
@@ -55,6 +48,3 @@ function App() {
 }
 
 export default App
-
-
-<Route path="/categories" element={<Categories />} />
